3ae16176e59f5326ad20278dc7250da517976418
abdulwaheedpasha <abdulwaheedpn@gmail.com>
2022-09-01 22:21:40 +0100

    nav bar update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
-import SearchBox from './SearchBox'
+import React from "react";
+import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
+import SearchBox from "./SearchBox";
 
 function Header() {
   return (
     <header>
-    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-      <Container>
-        <Navbar.Brand href="/">N Protein</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        
-          <Nav className="mr-auto">
-            <Nav.Link href="/dashboard">Home</Nav.Link>
-            <Nav.Link href="#link">Protein</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">N Protein</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
