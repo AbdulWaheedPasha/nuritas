f72c6195f4408f357e5b466bd7c31cb3e003dfd2
abdulwaheedpasha <abdulwaheedpn@gmail.com>
2022-09-01 09:53:25 +0100

    updated entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "running entrypoint.sh file"
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py populate_database proteins_time_course.csv
 exec "$@"
