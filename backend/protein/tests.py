6e797901420ab91014c1ddb746c35eb11301c86f
abdulwaheedpasha <abdulwaheedpn@gmail.com>
2022-09-02 09:54:25 +0100

    test case

--- backend/protein/tests.py
+++ backend/protein/tests.py
@@ -1,3 +1,26 @@
-# from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Protein
 
-# Create your tests here.
+import logging
+logger = logging.getLogger(__name__)
+
+class ProteinViewSetTests(APITestCase):
+    def test_get_protein(self):
+        """
+        Test get Protein
+        """
+        logger.debug('Starting test list persons')
+
+        url = 'http://127.0.0.1/api/protein/26'
+        logger.debug('Testing: %s'%url)
+        response = self.client.get(url, format='json')
+        print(response)
+        json = response.json()
+
+        logger.debug('Testing status code response: %s, code: %d'%(json, response.status_code))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        logger.debug('Testing result count')
+        self.assertEqual(len(json), 1)
